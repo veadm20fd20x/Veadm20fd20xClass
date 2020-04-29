3a71131cf195040d0793a922104589d4d785a764
Ole Egebjerg Mikkelsen <OEM@ucl.dk>
2020-04-29 23:00:18 +0200

    Add project files.

---
+++ Veadm20fd20xClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Veadm20fd20xClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
